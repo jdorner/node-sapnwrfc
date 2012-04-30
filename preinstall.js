748283d46b836d3a86b5046444f610fa64de8716
Joachim Dorner <jdorner@gmx.net>
2012-04-30 23:28:27 +0100

    Allow doing rebuilds

--- preinstall.js
+++ preinstall.js
@@ -1,32 +1,39 @@
+var child_process = require('child_process');
 var red = '\u001b[31m',
     green = '\u001b[32m',
     reset = '\u001b[0m';
 
-try {
-  var bindings = require('bindings')('sapnwrfc');
-  console.log(green + 'ok ' + reset + 'found precompiled module at ' + bindings.path);
-} catch (e) {
+var nodeGyp = function() {
   var os = require('os');
-  var child_process = require('child_process');
 
-  console.log(e);
-  console.log(red + 'error ' + reset + 'a precompiled module could not be found or loaded');
-  
-  // Spawn gyp
-  console.log(green + 'info ' + reset + 'trying to compile it...');
-  var opts = {};
-  opts.customFds = [ 0, 1, 2 ];
-  opts.env = process.env;
   switch (os.platform()) {
     case 'win32':
-      child_process.spawn('node-gyp.cmd', ['rebuild'], opts);
-      break;
+      return 'node-gyp.cmd';
     case 'linux':
     case 'darwin':
-      child_process.spawn('node-gyp', ['rebuild'], opts);
-      break;
     default:
-      console.log(red + 'error ' + reset + 'unknown platform: ' + os.platform());
-      break;
+      return 'node-gyp';
   }
 }
+
+var rebuild = function() {
+  try {
+    var bindings = require('bindings')('sapnwrfc');
+    console.log(green + 'ok ' + reset + 'found precompiled module at ' + bindings.path);
+  } catch (e) {
+    console.log(e);
+    console.log(red + 'error ' + reset + 'a precompiled module could not be found or loaded');
+    
+    // Spawn gyp
+    console.log(green + 'info ' + reset + 'trying to compile it...');
+    var opts = {};
+    opts.customFds = [ 0, 1, 2 ];
+    opts.env = process.env;
+    child_process.spawn(nodeGyp(), ['rebuild'], opts);
+  }
+}
+
+var opts = {};
+opts.customFds = [ 0, 1, 2 ];
+var cp = child_process.spawn(nodeGyp(), ['clean'], opts);
+cp.on('exit', rebuild);
