076096b67be500a3f97c423b158e42f0ca50b4db
Joachim Dorner <joachim.dorner@gmail.com>
2015-11-03 21:20:58 +0100

    fix commit message

--- appveyor-publish.bat
+++ appveyor-publish.bat
@@ -3,6 +3,6 @@ IF %appveyor_repo_branch%==master (
   git config user.name "jdorner"
   git pull origin %APPVEYOR_REPO_BRANCH%
   git add -f compiled/%NODE_VERSION_SHORT%/win32/x64/sapnwrfc.node
-  git commit -m "[ci skip] Add Windows binding for Node.js %NODE_VERSION%"
+  git commit -m "[ci skip] Add Windows binding for Node.js %NODE_VERSION_SHORT%"
   git push
 )
